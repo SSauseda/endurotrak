412b4afd6b5f37d048c607f0caadf63cf8cb0ae4
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-07-23 17:59:35 -0500

    ajusted signup route

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -64,6 +64,11 @@ def sign_up():
     if form.validate_on_submit():
         user = User(
             username=form.data['username'],
+            first_name=form.data['first_name'],
+            last_name=form.data['last_name'],
+            location=form.data['location'],
+            about=form.data['about'],
+            profile_image=form.data['profile_image'],
             email=form.data['email'],
             password=form.data['password']
         )
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -24,6 +24,11 @@ def password_matches(form, field):
     if field.data != form.password.data:
         raise ValidationError('Passwords do not match.')
 
+def validate_image_url(form, field):
+    if field.data:
+        if not (field.data.endswith('.jpg') or field.data.endswith('.jpeg') or field.data.endswith('.png')):
+            raise ValidationError('Please upload a valid image file (.jpg, .jpeg, .png)')
+
 
 class SignUpForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(message="Please create a username"), username_exists])
@@ -31,8 +36,7 @@ class SignUpForm(FlaskForm):
     last_name = StringField('last_name',validators=[DataRequired(message="Please enter your last name")])
     location = StringField('location',validators=[DataRequired(message="Please enter your location")])
     about = TextAreaField('about',validators=[DataRequired(message="Please enter a short bio"), Length(max=255)])
-    # profile_image = StringField('profile_image')
-    profile_image = FileField('profile_image', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+    profile_image = StringField('profile_image', validators=[validate_image_url])
     email = StringField('email', validators=[DataRequired(message="Please enter your email"), Email(message="Please enter a valid email address"), user_exists])
     password = PasswordField('password', validators=[DataRequired(message="Please create a password")])
     confirm_password = PasswordField('confirm_password', validators=[DataRequired(message="Please confirm your password"), password_matches])
