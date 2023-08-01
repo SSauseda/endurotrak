2226a9b0bb47e024bf94893705f1aab95e74bb21
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-08-01 07:24:46 -0500

    Added result form component

---
+++ react-app/src/components/CreateResultForm/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+const CreateResultForm = ({challenge}) => {
+
+
+    return (
+        <form>
+            <label>
+                <input type="text" name="title" />
+            </label>
+        </form>
+    )
+}
