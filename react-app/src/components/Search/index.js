bdf7517f6f3308f88803afba947fa9a203f3f15d
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-08-09 22:50:23 -0500

    debugging search bar

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { searchUsers } from '../../store/session';
 import './Search.css';
 
@@ -11,33 +11,48 @@ const UserSearch = () => {
     const [query, setQuery] = useState('');
 
     const searchResults = useSelector(state => state.session.searchResults);
+    console.log("SEARCH RESULTS", searchResults)
 
     const handleSearch = async (e) => {
         e.preventDefault();
+        console.log("SEARCHING", query)
         await dispatch (searchUsers(query));
     };
 
+    useEffect(() => {
+        console.log("SEARCH RESULTS", searchResults)
+    }, [searchResults])
+
     const handleUserClick = (userId) => {
-        history.push(`/athlete/${userId}`)
+        console.log("REDIRECT", userId)
+        history.push(`/challenges`)
     }
 
     return (
         <div>
-            <form onSubmit={handleSearch}>
-                <input
-                    className='search-bar' 
-                    type="text" 
-                    value={query} 
-                    onChange={e => setQuery(e.target.value)} 
-                    placeholder="Search for users..." 
-                />
-                <button className='search-button' type="submit">Search</button>
-            </form>
-            {searchResults && searchResults.map(user => (
-                <div key={user.id} onClick={() => handleUserClick(user.id)}>
-                    {user.first_name}
+            <div className="search-section">
+                <form onSubmit={handleSearch}>
+                    <input
+                        className='search-bar' 
+                        type="text" 
+                        value={query} 
+                        onChange={e => setQuery(e.target.value)} 
+                        placeholder="Search for users..." 
+                    />
+                    <button className='search-button' type="submit">Search</button>
+                </form>
+            </div>
+            
+            <div className="results-section">
+                {searchResults && searchResults.map(user => (
+                    <div 
+                        key={user.id} 
+                        onClick={() => handleUserClick(user.id)}
+                    >
+                        {user.first_name}
                     </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
