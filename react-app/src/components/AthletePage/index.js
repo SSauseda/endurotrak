44c2b274d01e2bdf7d1891f583e0212a57df53e9
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-08-16 22:09:21 -0500

    Able to view other users participating challenges on their own athlete page.

--- react-app/src/components/AthletePage/index.js
+++ react-app/src/components/AthletePage/index.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useHistory, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchSingleUser} from '../../store/session';
+import { fetchSingleUser } from '../../store/session';
+import { fetchUserChallenges } from '../../store/challenge';
 import './AthletePage.css';
 
 
 const AthletePage = () => {
     const { userId } = useParams();
+    const dispatch = useDispatch();
     const history = useHistory();
 
     const user = useSelector((state) => state.session.singleUser);
     const currentUser = useSelector((state) => state.session.user);
-    const challenges = useSelector((state) => Object.values(state.challenges));
-    console.log("ATHLETEPAGE", challenges)
-    const dispatch = useDispatch();
+
+
+
 
     useEffect(() => {
         if (currentUser && currentUser.id === parseInt(userId)) {
@@ -21,7 +23,12 @@ const AthletePage = () => {
             return;
         }
         dispatch(fetchSingleUser(userId));
+        dispatch(fetchUserChallenges(userId));
     }, [dispatch, userId, history, currentUser]);
+
+    const challenges = useSelector((state) => state.challenges);
+    const challengesArray = Object.values(challenges);
+    console.log('challenges', challengesArray)
 
     if (!user) {
         return <div>Loading...</div>
@@ -34,6 +41,27 @@ const AthletePage = () => {
                 <img className='athlete-picture' src={user.profileImage} alt="profile" />
             </div>
             <h1 className='athlete-name'>{user.firstName} {user.lastName}</h1>
+            <div className='user-challenge-container'>
+                <h2>Participating Challenges</h2>
+                <ul>
+                    {challengesArray.map(challenge => (
+                        <Link to={`/challenges/${challenge.id}`} key={challenge.id}>
+                        <div className="card-container">
+                            <img 
+                                className="card-img" 
+                                src={challenge.image_url} 
+                                alt="challenge"
+                                onError={(e) => {
+                                    e.target.onError = null;
+                                    e.target.src="https://t3.ftcdn.net/jpg/02/71/81/32/360_F_271813264_3GVBtWySh8y6ZgRoj8iWc9hXNcOMmzWf.jpg"
+                                }}
+                            />
+                            <div className="card-title">{challenge.title}</div>
+                        </div>
+                    </Link>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
